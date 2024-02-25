9d53ed1f739b16c325ee26bdf618a6f27c6c26e0
Faisal Ahmed Fahim <146206158+fahim-ahmed10@users.noreply.github.com>
2024-02-26 01:23:31 +0600

    availability functionality

--- js/selectionSeat.js
+++ js/selectionSeat.js
@@ -10,11 +10,32 @@ function getClickedElement(clicked_id, class1) {
   // If the element has the class, remove it; otherwise, add it
   if(hasClass1) {
     element.classList.remove("bg-[#1dd100]", "text-white", "hover:bg-[#33e214]");
-    const removeInnerText = getInnerText = '';
-    console.log(removeInnerText);
+    let removeInnerText = getInnerText = '';
+    console.log(removeInnerText + "removed");
+    increaseOneAvailableSeat();
   } else {
     element.classList.add("bg-[#1dd100]", "text-white", "hover:bg-[#33e214]");
-    const getInnerText = element.innerText;
+    let getInnerText = element.innerText; 
     console.log(getInnerText);
+    reduceOneAvailableSeat();
   }
+}
+
+//Reducing availablility by selection one seat
+function reduceOneAvailableSeat(){    
+    let seatLeft = document.getElementById("seatLeft");
+    let getInnerText  = seatLeft.innerText;
+    let convertToInt = parseInt(getInnerText);
+    let availableSeat = convertToInt - 1;
+    let showAvailableSeat = seatLeft.innerText = availableSeat;
+    console.log(getInnerText, availableSeat, showAvailableSeat);
+}
+
+//Increasing availablility by removing one seat from select
+function increaseOneAvailableSeat(){
+    let seatLeft = document.getElementById("seatLeft");
+    let getInnerText  = seatLeft.innerText;
+    let convertToInt = parseInt(getInnerText);
+    let availableSeat = convertToInt + 1;
+    let showAvailableSeat = seatLeft.innerText = availableSeat;
 }
